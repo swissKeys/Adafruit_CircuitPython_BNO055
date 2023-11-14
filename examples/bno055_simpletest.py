e9f2abd9fe900a2d525e18425b40d3efb988afaf
swisskeys <becki_barth@yahoo.com>
2023-11-14 10:16:20 +0100

    chnage sim

--- examples/bno055_simpletest.py
+++ examples/bno055_simpletest.py
@@ -1,14 +1,16 @@
 # SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
 # SPDX-License-Identifier: MIT
-
-import time
-import board
 import adafruit_bno055
 
+import time
 
-i2c = board.I2C()  # uses board.SCL and board.SDA
-# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
-sensor = adafruit_bno055.BNO055_I2C(i2c)
+import serial
+
+# Define the serial connection
+uart = serial.Serial('/dev/serial0', 115200)
+
+# Create an instance of the BNO055 sensor using UART
+sensor = adafruit_bno055.BNO055_UART(uart)
 
 # If you are going to use UART uncomment these lines
 # uart = board.UART()
