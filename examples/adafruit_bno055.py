1a6ca51782649deac352cceeadabaac3928f7a1e
swisskeys <becki_barth@yahoo.com>
2023-11-15 14:56:21 +0100

    chnaged read and write

--- examples/adafruit_bno055.py
+++ examples/adafruit_bno055.py
@@ -817,28 +817,32 @@ class BNO055_UART(BNO055):
     def _write_register(  # pylint: disable=arguments-differ,arguments-renamed
         self, register: int, data: int
     ) -> None:
+        ack=True
         if not isinstance(data, bytes):
             data = bytes([data])
-        self._uart.write(bytes([0xAA, 0x00, register, len(data)]) + data)
-        now = time.monotonic()
-        while self._uart.in_waiting < 2 and time.monotonic() - now < 0.25:
-            pass
-        resp = self._uart.read(self._uart.in_waiting)
-        if len(resp) < 2:
-            raise OSError("UART access error.")
-        if resp[0] != 0xEE or resp[1] != 0x01:
-            raise RuntimeError(f"UART write error: {resp[1]}")
+                    # UART write.
+            command = bytearray(4 + len(data))
+            command[0] = 0xAA  # Start byte
+            command[1] = 0x00  # Write
+            command[2] = register & 0xFF
+            command[3] = len(data) & 0xFF
+            command[4:] = map(lambda x: x & 0xFF, data)
+            
+            # Send the UART command with automatic acknowledgment handling.
+            resp = self._serial_send(command, ack=ack)
+            
+            # Verify register write succeeded if there was an acknowledgement.
+            if ack and (resp[0] != 0xEE or resp[1] != 0x01):
+                raise RuntimeError(f"UART write error: {resp[1]}")
 
     def _read_register(  # pylint: disable=arguments-differ
         self, register: int, length: int = 1
     ) -> int:
+        
         i = 0
         while i < 3:
-            self._uart.write(bytes([0xAA, 0x01, register, length]))
-            now = time.monotonic()
-            while self._uart.in_waiting < length + 2 and time.monotonic() - now < 0.1:
-                pass
-            resp = self._uart.read(self._uart.in_waiting)
+            command = bytearray([0xAA, 0x01, register, length])
+            resp = self._serial_send(command)
             if len(resp) >= 2 and resp[0] == 0xBB:
                 break
             i += 1
@@ -860,9 +864,9 @@ class BNO055_UART(BNO055):
         attempts = 0
         while True:
             # Flush any pending received data to get into a clean state.
-            self._serial.flushInput()
+            self._uart.flushInput()
             # Send the data.
-            self._serial.write(command)
+            self._uart.write(command)
             # Stop if no acknowledgment is expected.
             if not ack:
                 return
