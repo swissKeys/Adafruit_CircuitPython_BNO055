e4cbfa451f41c03ec723620e91de7d202c4645a6
swisskeys <becki_barth@yahoo.com>
2023-11-15 16:11:20 +0100

    magOnly

--- examples/bno055_simpletest.py
+++ examples/bno055_simpletest.py
@@ -102,11 +102,11 @@ def collect_array(measured_axis, number_of_datapoints, length_of_one_side, check
 
     bno_changed = threading.Condition()
 
-    while mag != 3:
-        with bno_changed:
-            system, gyro, accel, mag = sensor.calibration_status
-            print(system, gyro, accel, mag)
-        time.sleep(1.0/10)
+    """     while mag != 3:
+            with bno_changed:
+                system, gyro, accel, mag = sensor.calibration_status
+                print(system, gyro, accel, mag)
+            time.sleep(1.0/10) """
 
     print(system, gyro, accel, mag)
 
