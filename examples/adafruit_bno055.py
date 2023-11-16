3b0d0f22f1b0108e8a835defa07650cabce06328
swisskeys <becki_barth@yahoo.com>
2023-11-16 14:02:56 +0100

    remove log

--- examples/adafruit_bno055.py
+++ examples/adafruit_bno055.py
@@ -839,7 +839,6 @@ class BNO055_UART(BNO055):
             self._uart.write(bytes([0xAA, 0x01, register, length]))
             now = time.monotonic()
             while self._uart.in_waiting < length + 2 and time.monotonic() - now < 0.1:
-                print('while in waiting smaller one:', self._uart.in_waiting)
                 pass
             print('in loop', self._uart.in_waiting)
             resp = self._uart.read(self._uart.in_waiting)
